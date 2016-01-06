7c2eb182ce6542c43bdb92f1a52668b68bcb3804
iAndroidCollege <collegeandroid2015@gmail.com>
2016-01-06 18:09:07 +0200

    Initial commit

---
+++ app/src/main/java/college/edu/tomer/tabsdemo/TabsPagerAdapter.java
@@ -0,0 +1,45 @@
+package college.edu.tomer.tabsdemo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by master on 06/01/16.
+ */
+public class TabsPagerAdapter extends FragmentStatePagerAdapter {
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new SendMailFragment();
+            case 1:
+                return new RecievedMailFragment();
+            case 2:
+                return new SettingsFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "SendMail";
+            case 1:
+                return  "RecievedMail";
+            case 2:
+                return "Settings";
+        }
+        return null;
+    }
+}
